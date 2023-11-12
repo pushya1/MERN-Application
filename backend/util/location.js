1827681a6252c96a881da027e135653269790917
pushya1 <ss123maila@gmail.com>
2023-11-13 02:37:10 +0530

    first commit

---
+++ backend/util/location.js
@@ -0,0 +1,9 @@
+// const getCoordsForAddress = (address) => {
+//   console.log("recived address in coords function " + address);
+//   return {
+//     lat: 40.7484474,
+//     lng: -73.9871516,
+//   };
+// };
+
+// export default getCoordsForAddress;
